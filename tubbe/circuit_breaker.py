5bf40a1afd329d1f23b7f1dd33ba396eba483d0e
renpenghua <renpenghua@daixiaomi.com>
2018-03-22 12:44:02 +0000

    add switch cb

--- tubbe/circuit_breaker.py
+++ tubbe/circuit_breaker.py
@@ -4,6 +4,8 @@
 
 import abc
 
+from switch_client.base import get_switch_info
+
 
 class AbstractCircuitBreaker(object):
 
@@ -33,3 +35,14 @@ class DefaultCircuitBreaker(AbstractCircuitBreaker):
 
     def break_or_not(self, counter):
         return not counter.is_available()
+
+
+class SwitchCircuitBreaker(AbstractCircuitBreaker):
+    ''' switch service based circuit breaker '''
+
+    def break_or_not(self, switch_name):
+        val = get_switch_info(switch_name, {u'on': 100, u'off': 0})
+        try:
+            return val[u'off'] == 100
+        except:
+            return False
